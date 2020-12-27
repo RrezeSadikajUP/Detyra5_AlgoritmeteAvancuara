c41af93234027005c8b4d3b71f3a0348f9b841d5
RrezeSadikajUP <75950223+RrezeSadikajUP@users.noreply.github.com>
2020-12-27 23:08:31 +0100

    Add files via upload

---
+++ BoyerMooreHorspool.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Horspool
+{
+	public static class BoyerMooreHorspool
+	{
+		public static int Find(string text, string pattern)
+		{
+			// If substring is bigger than string, no match exists
+			if (pattern.Length > text.Length)
+				return -1;
+
+			// If the substring has characters not in the string, no match exists
+			if (pattern.Except(text).Any())
+				return -1;
+
+			Dictionary<char, int> BadMatchTable = new Dictionary<char, int>();
+
+			// Initializes every letter with the default value
+			foreach (char c in text)
+			{
+				if (!BadMatchTable.Keys.Contains(c))
+					BadMatchTable.Add(c, pattern.Length);
+			}
+
+			// The formula for every letter in the pattern (except the last) is lenght - index - 1
+			for (int i = 0; i < pattern.Length - 1; i++)
+			{
+				BadMatchTable[pattern[i]] = pattern.Length - i - 1;
+
+			}
+
+			int index = 0;
+
+			while (index < text.Length - pattern.Length)
+			{
+				bool match = true;
+
+				for (int i = pattern.Length - 1; i >= 0; i--)
+				{
+					if (pattern[i] != text[index + i])
+					{
+						match = false;
+						index += BadMatchTable[text[index + pattern.Length - 1]];
+						if (index >= text.Length)
+						{
+							break;
+						}
+					}
+				}
+
+				if (match)
+					return index;
+			}
+
+			return -1;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Horspool
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			//Apply for 5 tests
+			for (int i = 0; i < 6; i++)
+			{
+				Test();
+				Console.ReadKey();
+
+			}
+		}
+
+		public static void Test()
+		{
+			Console.WriteLine("Shënoni një fjalë/fjali për t'u përshkuar - text:");
+			string text = Console.ReadLine();
+
+			Console.WriteLine();
+
+			Console.WriteLine("Shënoni një fjalë/fjali kërkuese - pattern:");
+			string pattern = Console.ReadLine();
+
+			int match = BoyerMooreHorspool.Find(text, pattern);
+
+			Console.Clear();
+
+			if (match == -1)
+			{
+				Console.WriteLine("Asnjë përshtatje nuk u gjend!");
+				return;
+			}
+
+			Console.WriteLine("Përshtatja në karakterin {0}", match);
+
+			Console.WriteLine();
+			Console.WriteLine(text);
+			for (int i = 0; i < match; i++)
+				Console.Write(" ");
+
+			for (int i = 0; i < pattern.Length; i++)
+				Console.Write("^");
+
+		}
+	}
+}
